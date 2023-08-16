1b258d3d4f89a137fa58c2159e0a4b92ead703ae
Jan Pierre Sanchez <janpierre.sanchez@halopowered.com>
2023-08-16 13:32:25 -0500

    feat(ui): 🎸 create service to get news

--- src/app/modules/home/pages/news/news.component.ts
+++ src/app/modules/home/pages/news/news.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { News } from '@home/models';
+import { NewsService } from '@home/services';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss'],
 })
-export class NewsComponent {}
+export class NewsComponent {
+  news$: Observable<News> = this._newsServices.getNews();
+
+  constructor(private _newsServices: NewsService) {}
+}
