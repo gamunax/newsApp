36b728d93951950afa7cdd55c15818dab0d36126
Jan Pierre Sanchez <janpierre.sanchez@halopowered.com>
2023-08-16 15:33:45 -0500

    feat(ui): 🎸 set url to redirection

--- src/app/modules/home/components/news-card/news-card.module.ts
+++ src/app/modules/home/components/news-card/news-card.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NewsCardComponent } from './news-card.component';
 
 @NgModule({
   declarations: [NewsCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NewsCardComponent],
 })
 export class NewsCardModule {}
